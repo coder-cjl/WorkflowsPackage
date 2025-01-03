12d9ddc161ad6cf1cd326c0e435d102ac285d5ec
雷子 <cjlsire@126.com>
2025-01-03 14:49:11 +0800

    5.10.0-1

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.10.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["WorkflowsPackage"]),
     ],
     dependencies: [
-           .package(url: "https://github.com/swiftlang/swift-testing.git", from: "6.0.0"),
+           .package(url: "https://github.com/swiftlang/swift-testing.git", from: "0.11.0"),
        ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
