376faaf88e348015b08e86b1bb934a6780d9cabb
雷子 <cjlsire@126.com>
2025-01-03 10:57:02 +0800

    first commit

---
+++ Sources/WorkflowsPackage/Person.swift
@@ -0,0 +1,23 @@
+//
+//  Person.swift
+//  WorkflowsPackage
+//
+//  Created by 雷子 on 2025/1/3.
+//
+
+import Foundation
+
+class Person {
+    
+    func sayA() -> String {
+        return "a"
+    }
+    
+    func sayB() -> String {
+        return "b"
+    }
+    
+    func sayC() -> String {
+        return "c"
+    }
+}
---
+++ Tests/WorkflowsPackageTests/WorkflowsPackageTests.swift
@@ -0,0 +1,27 @@
+import Testing
+@testable import WorkflowsPackage
+
+@Test func example() async throws {
+}
+
+@Test func example2() async throws {
+    
+    let person = Person()
+    let result = person.sayA()
+    #expect(result == "a")
+}
+
+
+@Test func example3() async throws {
+    
+    let person = Person()
+    let result = person.sayB()
+    #expect(result == "b")
+}
+
+@Test func example4() async throws {
+    
+    let person = Person()
+    let result = person.sayC()
+    #expect(result == "C")
+}
